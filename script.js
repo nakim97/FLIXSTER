0a3b5ebe3c9268ad4a84d7d816675a58612995b9
Stella Kim <62909702+nakim97@users.noreply.github.com>
2021-06-08 23:12:04 -0700

    implement now playing and search movies features

---
+++ script.js
@@ -0,0 +1,66 @@
+const api_url = 'https://api.themoviedb.org/3'
+const api_key = '831004296f374b1a0723bb4e809b453a'
+const image_url = 'https://image.tmdb.org/t/p'
+const search_url = 'https://api.themoviedb.org/3/search/movie?api_key={api_key}&query='
+
+const searchQuery = document.getElementById("search").value;
+const searchKey = document.getElementById("search");
+const displayMovies = document.getElementById("search-movies");
+
+const nowPlaying = document.getElementById('now-playing');
+const nowPlayingDiv = document.getElementById("now-playing");
+
+// function to implement search bar
+document.getElementById('search').addEventListener('keyup', function(event){
+    if(event.keyCode ===13){
+        const searchQuery = document.getElementById("search").value;
+        event.preventDefault();
+        console.log(searchQuery);
+        nowPlayingDiv.style.display ="none";
+        searchMovies();
+    }
+  })
+
+
+
+// function to display Now Playing movies on homepage
+async function nowPlayingMovies(){
+    const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}`);
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+    let movieContainer = "";
+    jsonResponse.results.map((result) =>
+    movieContainer += `
+    <div id= "movie-container">
+        <img src="https://image.tmdb.org/t/p/w342${result.poster_path}">
+        <h3>${result.title}</h3>
+        <h3>⭐${result.vote_average}</h3>
+    </div>
+    `
+    )
+    nowPlaying.innerHTML = movieContainer;
+}
+
+
+
+// function to display searched Movies 
+async function searchMovies(){
+    const searchQuery = document.getElementById("search").value;
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${searchQuery}`);
+    const jsonResponse = await response.json();
+    let searchContainer = "";
+    jsonResponse.results.map((result) =>
+    searchContainer += `
+    <div id= "movie-container">
+        <img src="https://image.tmdb.org/t/p/w342${result.poster_path}">
+        <h3>${result.title}</h3>
+        <h3>⭐${result.vote_average}</h3>
+    </div>
+    `
+    )
+    displayMovies.innerHTML = searchContainer;
+
+}
+
+// Display Now Playing Movies function
+nowPlayingMovies();
